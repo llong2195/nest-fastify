f2eedf032c8a3f9cf6bbec1ac3a3c810ddc21f7a
llong2195 <nduylong9501@gmail.com>
2023-02-03 14:04:22 +0700

    refactor

--- src/commander.ts
+++ src/commander.ts
@@ -1,11 +1,11 @@
 import { CommandFactory } from 'nest-commander';
-// import { CommanderModule } from '@src/modules/commander/commander.module';
+import { CommanderModule } from '@src/modules/commander/commander.module';
 import { LogLevel } from '@nestjs/common';
-import { AppModule } from './app.module';
+// import { AppModule } from './app.module';
 
 async function bootstrap() {
     const logLevelsDefault: LogLevel[] = ['log', 'error', 'warn', 'debug', 'verbose'];
-    await CommandFactory.run(AppModule, logLevelsDefault);
+    await CommandFactory.run(CommanderModule, logLevelsDefault);
 }
 
 bootstrap();
