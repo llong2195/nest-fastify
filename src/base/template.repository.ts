077f7460f12432f55ce02b5bda026b3a699f6c47
llong2195 <nduylong9501@gmail.com>
2023-02-15 11:37:48 +0700

    refactor

--- src/base/template.repository.ts
+++ src/base/template.repository.ts
@@ -1,7 +1,7 @@
 import { DataSource, Repository } from 'typeorm';
 
 import { Injectable } from '@nestjs/common';
-import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '@src/modules/user/entities/user.entity';
 
 /**
@@ -10,10 +10,18 @@ import { UserEntity } from '@src/modules/user/entities/user.entity';
  **/
 @Injectable()
 export class TemplateRepository extends Repository<UserEntity> {
-    constructor(
-        @InjectDataSource() private dataSource: DataSource,
-        @InjectRepository(UserEntity) userRepo: Repository<UserEntity>,
-    ) {
+    constructor(@InjectRepository(UserEntity) userRepo: Repository<UserEntity>) {
         super(userRepo.target, userRepo.manager, userRepo.queryRunner);
+    }
+}
+
+/**
+ * Template Custom Repository: TemplateRepository extends Repository<Entity>
+ *
+ **/
+@Injectable()
+export class Template2Repository extends Repository<UserEntity> {
+    constructor(private dataSource: DataSource) {
+        super(UserEntity, dataSource.manager);
     }
 }
