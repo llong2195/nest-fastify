12df4835a57a6e262c6ce6112594be3cffdd7436
llong2195 <nduylong9501@gmail.com>
2023-02-16 11:58:01 +0700

    refactor: i18n

--- src/i18n/i18n.module.ts
+++ src/i18n/i18n.module.ts
@@ -2,17 +2,19 @@ import { Global, Module } from '@nestjs/common';
 
 import { I18nService } from './i18n.service';
 
+const I18N_SERVICE = 'I18N_SERVICE';
+
 @Global()
 @Module({
     providers: [
         {
-            provide: 'I18N_SERVICE',
+            provide: I18N_SERVICE,
             useFactory: () => I18nService,
         },
     ],
     exports: [
         {
-            provide: 'I18N_SERVICE',
+            provide: I18N_SERVICE,
             useFactory: () => I18nService,
         },
     ],
