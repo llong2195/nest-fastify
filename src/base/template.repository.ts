2b28f3440f5e885867cd375981309fbde9573e1e
llong2195 <nduylong9501@gmail.com>
2023-02-17 11:19:49 +0700

    feat: template repository extend BaseRepository

--- src/base/template.repository.ts
+++ src/base/template.repository.ts
@@ -3,6 +3,7 @@ import { DataSource, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '@src/modules/user/entities/user.entity';
+import { BaseRepository } from './base.repository';
 
 /**
  * Template Custom Repository: TemplateRepository extends Repository<Entity>
@@ -23,5 +24,16 @@ export class TemplateRepository extends Repository<UserEntity> {
 export class Template2Repository extends Repository<UserEntity> {
     constructor(private dataSource: DataSource) {
         super(UserEntity, dataSource.manager);
+    }
+}
+
+/**
+ * Template Custom Repository: TemplateRepository extends BaseRepository<Entity>
+ *
+ **/
+@Injectable()
+export class Template3Repository extends BaseRepository<UserEntity> {
+    constructor(@InjectRepository(UserEntity) repository: Repository<UserEntity>) {
+        super(repository);
     }
 }
