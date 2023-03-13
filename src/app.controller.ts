5b4738a90dcd3b581573d05d24d33dd1e3b0c903
llong2195 <nduylong9501@gmail.com>
2023-03-13 16:53:15 +0700

    refactor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,23 @@
 import { Request } from 'express';
 
 import { ValidateError } from '@exceptions/errors/index';
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Inject, Req } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
-import { AppService } from './app.service';
+import { BaseResponseDto } from '@base/base.dto';
 import { ErrorMessageCode } from './constants';
 import { LoggerService } from './logger/custom.logger';
+import { I18N_SERVICE } from './i18n/i18n.module';
+import { I18nService } from './i18n/i18n.service';
 
 @ApiTags('/')
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService, private logger: LoggerService) {}
+    constructor(private logger: LoggerService, @Inject(I18N_SERVICE) private i18n: I18nService) {}
 
     @Get()
-    getHello(): string {
-        return this.appService.getHello();
+    getHello() {
+        return 'hello';
     }
 
     @Get('/profile')
@@ -30,7 +32,7 @@ export class AppController {
 
     @Get('exceptions')
     TestException(): any {
-        throw new ValidateError(ErrorMessageCode.INVALID, 400, 9000);
+        throw new ValidateError(ErrorMessageCode.INVALID, 400);
     }
 
     @Get('healthz')
