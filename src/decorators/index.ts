d3578f9ba1cde1ca994b93a5b7c9d724a3be7c39
Nguyen Duy Long <68091468+llong2195@users.noreply.github.com>
2023-11-30 22:15:46 +0700

    fix: fix err

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -2,6 +2,5 @@ export * from './authorize.decorator';
 export * from './current.user.decorator';
 export * from './header.decorator';
 export * from './ip.address.decorator';
-export * from './property.decorator';
 export * from './public.decorator';
 export * from './role.decorators';
